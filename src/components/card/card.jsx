13c5af7c79d9504e2d80cd28bbe838ccfdf64996
Ivan Kornev <chichinumber222@gmail.com>
2020-07-31 19:41:06 +0300

    margins, fixes

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,20 +5,31 @@ import travelTime from '../../services/travel-time';
 import calculateStops from '../../services/calculate-stops';
 
 function Card({ price, carrier, segments }) {
-  const wayThere = `${segments[0].origin}-${segments[0].destination}`;
-  const wayBack = `${segments[1].origin}-${segments[1].destination}`;
-  
+  const ways = segments.map((way) => {
+    return (
+      <>
+        <HeadAndValue 
+          head={`${way.origin}-${way.destination}`} 
+          value={travelTime(way.date, way.duration)}
+        />
+        <HeadAndValue 
+          head='В ПУТИ' 
+          value={minToHours(way.duration)}
+        />
+        <HeadAndValue 
+          head={calculateStops(way.stops)} 
+          value={way.stops.join(', ')}
+        />
+      </>
+    )
+  })
+
   return (
     <div className={styles.card}>
-      <p className={styles.price}>{price} Р</p>
+      <p className={styles.price}>{price.toLocaleString('ru-RU')} Р</p>
       <div></div>
       <img alt={`logo ${carrier}`} src={`//pics.avs.io/99/36/${carrier}.png`}/>
-      <HeadAndValue head={wayThere} value={travelTime(segments[0].date, segments[0].duration)}/>
-      <HeadAndValue head='В ПУТИ' value={minToHours(segments[0].duration)}/>
-      <HeadAndValue head={calculateStops(segments[0].stops)} value={segments[0].stops.join(', ')}/>
-      <HeadAndValue head={wayBack} value={travelTime(segments[1].date, segments[1].duration)}/>
-      <HeadAndValue head='В ПУТИ' value={minToHours(segments[1].duration)}/>
-      <HeadAndValue head={calculateStops(segments[1].stops)} value={segments[1].stops.join(', ')}/>
+      {ways}
     </div>
   )
 }
