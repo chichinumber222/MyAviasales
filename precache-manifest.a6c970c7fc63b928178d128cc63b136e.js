9914174b494f95118cc0000e58c1729bf7747a25
Ivan Kornev <chichinumber222@gmail.com>
2020-08-05 15:03:58 +0300

    Updates

---
+++ precache-manifest.a6c970c7fc63b928178d128cc63b136e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47933c8dd3c3f5d1e3ab985b800af15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46ebe880d127cd75ede5",
+    "url": "./static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "ff1f263965a56fff9ff4",
+    "url": "./static/css/main.a716ed01.chunk.css"
+  },
+  {
+    "revision": "46ebe880d127cd75ede5",
+    "url": "./static/js/2.a273cb03.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.a273cb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1f263965a56fff9ff4",
+    "url": "./static/js/main.2e90506b.chunk.js"
+  },
+  {
+    "revision": "d78931731f4b1b474aea",
+    "url": "./static/js/runtime-main.a3fa4abd.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "./static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
